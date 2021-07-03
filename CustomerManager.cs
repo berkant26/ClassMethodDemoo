32df25d5a18b98216c062375cd769c10c95fc595
berkant26 <youbeko26@gmail.com>
2021-07-03 18:00:12 +0300

    Initial commit

---
+++ CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemoo
+{
+    class CustomerManager
+    {
+        public void Add(String Name, int Id)
+        {
+            Console.WriteLine("Customer Added!!" + " " + "Id:" + Id + " " + "Name: " + Name);
+        }
+
+        public void Delete( String Name)
+        {
+            Console.WriteLine("Customer Deleted"+" " +Name);
+        }
+        
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMethodDemoo
+{
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            Customers customer1 = new Customers();
+            customer1.Id = 1;
+            customer1.Name = "berkant";
+            customer1.Age = 20;
+
+            Customers customer2 = new Customers();
+            customer2.Id = 2;
+            customer2.Name = "murat";
+            customer2.Age = 24;
+
+
+            Customers[] customers = new Customers[] {customer1,customer2};
+
+
+            foreach (var item in customers)
+            {
+                Console.WriteLine("Customer name: " + item.Name + " " + "Customer Id :" + item.Id);
+            }
+
+
+
+                CustomerManager customerManager = new CustomerManager();
+                customerManager.Add("Hasan",9);
+                 CustomerManager customerManager1 = new CustomerManager();
+            customerManager1.Delete("Hasan");
+           
+
+
+
+        }
+    }
+}
